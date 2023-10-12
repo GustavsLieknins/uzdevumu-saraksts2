e781bf39b6651ae39d06203729d3c657e9719a86
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-12 09:54:43 +0300

    added

--- src/App.js
+++ src/App.js
@@ -1,47 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
 import Todo from './ToDo.js';
+import {useState} from 'react';
+import {useEffect} from 'react';
+
 
 function App() {
+  
+  const url = 'https://jsonplaceholder.typicode.com/todos';
+  const [todos, setTodos] = useState([]);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setTodos(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchTodos();
+  }, [])
   const fullLietinasParts = [
     {
       userId: 1,
       id: 1,
       title: "delectus aut autem",
-      completed: "false"
+      completed: false
     },
     {
       userId: 1,
       id: 2,
       title: "quis ut nam facilis et officia qui",
-      completed: "false"
+      completed: false
     },
       {
         userId: 1,
         id: 3,
         title: "fugiat veniam minus",
-        completed: "false"
+        completed: false
         },
         {
           userId: 1,
           id: 4,
           title: "et porro tempora",
-          completed: "true"
+          completed: true
           },
           {
             userId: 1,
             id: 5,
             title: "laboriosam mollitia et enim quasi adipisci quia provident illum",
-            completed: "false"
+            completed: false
             },
             {userId: 1,
               id: 6,
               title: "qui ullam ratione quibusdam voluptatem quia omnis",
-              completed: "false"
+              completed: false
               }
   ];
-  const visasLietas = fullLietinasParts.map((lietas, i) => {
-    return <Todo key={i} title={lietas.title} userId={lietas.id} checkedornoter={lietas.completed}/>
+  const visasLietas = todos.map((lietas, i) => {
+    return <Todo key={i} title={lietas.title} userId={lietas.id} checkedornoter={lietas.completed} />
   });
   return (
     <>
--- src/ToDo.js
+++ src/ToDo.js
@@ -3,12 +3,18 @@ function ToDo(props) {
     //   const stundasJSX = props.stundas.map((stunda, i) => {
     //     return <Stunda key={i} nosaukums={stunda} />
     //   });
+    const [checked, setChecked] = useState(props.checkedornoter);
+    function ChangedTrueFalse(e){
+      setChecked(!checked)
+      console.log("Inputs " + props.userId + "tika parversts par " + checked );
+
+    }
 
       return (
       <>
       <div>
         <h2>{props.userId}</h2>
-        <label>{props.title}<input type="radio" name="mainradio" checked={props.checkedornoter} /></label>
+        <label>{props.title}<input type="checkbox"  defaultChecked={checked} onChange={event => ChangedTrueFalse(event.target.value)}/></label>
         </div>
       </>
       )
